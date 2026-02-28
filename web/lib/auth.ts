6eaa5eda40b5777ee8ad85759aba612c2df8dc54
Daniel <167367785+u-iDaniel@users.noreply.github.com>
2026-02-28 14:28:18 -0700

    Initial better-auth config

---
+++ web/lib/auth.ts
@@ -0,0 +1,18 @@
+import { betterAuth } from "better-auth";
+import { Pool } from "pg";
+
+export const auth = betterAuth({
+  database: new Pool({
+    connectionString: process.env.DATABASE_URL,
+  }),
+  emailAndPassword: {
+    enabled: true,
+    // Add email verification later (prob with AWS SES): https://www.better-auth.com/docs/authentication/email-password#email-verification
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+  },
+});
